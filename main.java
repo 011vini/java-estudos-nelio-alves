0928da75ac43427cb472621a36c8a6b824e71689
Vinicio Cauã <vinicio.caua.silava@gmail.com>
2025-08-05 21:51:13 -0300

    Adicionar conteudos aprendidos hoje.

--- main.java
+++ main.java
@@ -1,15 +1,21 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
             String x;
+            int y;
+            double z;
             x = sc.next();
-            System.out.println("Você digitou " + x);
-
-
-
+            y = sc.nextInt();
+            z = sc.nextDouble();
+            System.out.println("dados digitados: ");
+            System.out.println(x);
+            System.out.println(y);
+            System.out.println(z);
 
         sc.close();
     }
